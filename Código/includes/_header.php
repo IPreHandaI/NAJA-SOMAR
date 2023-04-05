5264770da4b68cd68b59e29f577b22fa166f18ff
Ana Luiza <yanaluiza26y@gmail.com>
2023-04-05 20:45:57 -0300

    dropdown

---
+++ Código/includes/_header.php
@@ -0,0 +1,25 @@
+<?php
+include_once '../includes/_head.php';
+?>
+
+<header>
+
+    <div class="colsheader">
+        <div class="headercol headercol1">Coluna 1</div>
+        <div class="headercol headercol2">
+            <div class="dropdown">
+                <button class="dropbtn">Dropdown</button>
+                <div class="dropdown-content">
+                    <a href="#">Link 1</a>
+                    <a href="#">Link 2</a>
+                    <a href="#">Link 3</a>
+                </div>
+            </div>
+        </div>
+        <div class="headercol headercol3">Coluna 3</div>
+        <div class="headercol headercol4">Coluna 4</div>
+        <div class="headercol headercol5">Coluna 5</div>
+    </div>
+    
+</header>
+
