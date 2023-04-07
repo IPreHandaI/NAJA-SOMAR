fb71129cd9d0f321732ccf277bdd79e308037238
Ana Luiza <yanaluiza26y@gmail.com>
2023-04-06 22:12:40 -0300

    animaçãoHeader

--- Código/includes/_header.php
+++ Código/includes/_header.php
@@ -5,20 +5,56 @@ include_once '../includes/_head.php';
 <header>
 
     <div class="colsheader">
-        <div class="headercol headercol1">Coluna 1</div>
+        <div class="headercol headercol1">
+        <div class="dropdown">
+                <button class="dropbtn">Dropdown</button>
+                <div class="dropdown-content">
+                    <a class="link1" href="#">Link 1</a>
+                    <a class="link2" href="#">Link 2</a>
+                    <a class="link3" href="#">Link 3</a>
+                </div>
+            </div>
+        </div>
         <div class="headercol headercol2">
             <div class="dropdown">
                 <button class="dropbtn">Dropdown</button>
                 <div class="dropdown-content">
                     <a class="link1" href="#">Link 1</a>
-                    <a id="link2" href="#">Link 2</a>
-                    <a id="link3" href="#">Link 3</a>
+                    <a class="link2" href="#">Link 2</a>
+                    <a class="link3" href="#">Link 3</a>
                 </div>
             </div>
         </div>
-        <div class="headercol headercol3">Coluna 3</div>
-        <div class="headercol headercol4">Coluna 4</div>
-        <div class="headercol headercol5">Coluna 5</div>
+        <div class="headercol headercol3">
+            <div class="dropdown">
+                <button class="dropbtn">Dropdown</button>
+                <div class="dropdown-content">
+                    <a class="link1" href="#">Link 1</a>
+                    <a class="link2" href="#">Link 2</a>
+                    <a class="link3" href="#">Link 3</a>
+                </div>
+            </div>
+        </div>
+        <div class="headercol headercol4">
+            <div class="dropdown">
+                <button class="dropbtn">Dropdown</button>
+                <div class="dropdown-content">
+                    <a class="link1" href="#">Link 1</a>
+                    <a class="link2" href="#">Link 2</a>
+                    <a class="link3" href="#">Link 3</a>
+                </div>
+            </div>
+        </div>
+        <div class="headercol headercol5">
+            <div class="dropdown">
+                <button class="dropbtn">Dropdown</button>
+                <div class="dropdown-content">
+                    <a class="link1" href="#">Link 1</a>
+                    <a class="link2" href="#">Link 2</a>
+                    <a class="link3" href="#">Link 3</a>
+                </div>
+            </div>
+        </div>
     </div>
     
 </header>
