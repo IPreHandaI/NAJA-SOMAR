68f037d9b6d3386ca1e85038facaf458114499df
nicolasdiovani <93234639+nicolasdiovani@users.noreply.github.com>
2023-04-14 14:41:21 -0300

    Add files via upload

---
+++ Código/login.php
@@ -0,0 +1,21 @@
+<?php
+include_once 'includes/_head.php'
+include_once 'includes/_header.php'
+?>
+    <main>
+        <div id="box-pai">
+            <h1 style="color:#8B8B8B;" >Login</h1>
+            <h3 style="color:#8B8B8B;">Lorem ipsum dolor, sit amet consectetur </h3>
+            <input type="text" name="" id="" class="caixa-texto">
+            <input type="password" name="" id="" class="caixa-texto">
+            <input type="button" value="clicar" id="botão">
+            <hr>
+            <p>or with</p>
+            <input type="text" name="" id="" class="caixa-texto">
+            <input type="password" name="" id="" class="caixa-texto">
+            <h6 style="color:#8B8B8B; margin-top: 15px;">Don’t have an accont yet? <a style="color:#444444;">Sing Up</a></h6>
+        </div>
+    </main>
+<?php
+include_once 'includes/_footer.php'
+?>
