d6f10f0258bcd1ed7f6de6e63df51f723139b0ae
nicolasdiovani <93234639+nicolasdiovani@users.noreply.github.com>
2023-04-14 16:38:44 -0300

    Update _header.php

--- Código/includes/_header.php
+++ Código/includes/_header.php
@@ -3,55 +3,8 @@ include_once 'includes/_head.php';
 ?>
 
 <header>
-    <div class="colsheader">
-        <div class="headercol headercol1">
-            <div>
-                <img id="logo" src="imagens/sorriso.png">
-            </div>
-        </div>
-        <div class="headercol headercol2">
-            <div class="dropdown1">
-            <div class="dropdown-header1">
-                <a>Selecione</a>
-            </div>
-                <div class="dropdown-options2">
-                    <div class="dropdown-option2 option1" value="opcao1">
-                        <p id="p">Opção 1</p>    
-                    </div>
-                    <div class="dropdown-option2 option2" value="opcao2">
-                        <p>Opção 2</p>
-                    </div>
-                    <div class="dropdown-option2 option3" value="opcao3">
-                        <p>Opção 3</p>
-                    </div>
-                    <div class="dropdown-option2 option4" value="opcao4">
-                        <p>Opção 4</p>
-                    </div>
-                </div>            
-            </div>
-        </div>
-        <div class="headercol headercol2">
-            <div class="dropdown2">
-                <div class="dropdown-header2">
-                    <a>Selecione</a>
-                </div>
-                <div class="dropdown-options2">
-                    <div class="dropdown-option2 option1" value="opcao1">
-                        <p id="p">Opção 1</p>    
-                    </div>
-                    <div class="dropdown-option2 option2" value="opcao2">
-                        <p>Opção 2</p>
-                    </div>
-                    <div class="dropdown-option2 option3" value="opcao3">
-                        <p>Opção 3</p>
-                    </div>
-                    <div class="dropdown-option2 option4" value="opcao4">
-                        <p>Opção 4</p>
-                    </div>
-                </div>            
-            </div>
-        </div>
-    </div>
-
+    <img src="imagens/menu.png"id="menu" alt="">
+    <img src="imagens/meta.png"id="metas" alt="">
+    <img src="imagens/perfil.png"id="perfil" alt="">
 </header>
 <main>
