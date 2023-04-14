4b403b6bd3395ae422565fdcb97f9895b6e19e9a
Ana Luiza <yanaluiza26y@gmail.com>
2023-04-14 16:24:05 -0300

    começoFooter

--- Código/includes/_footer.php
+++ Código/includes/_footer.php
@@ -1,51 +1,20 @@
 <?php
-    include_once 'includes/_head.php';
+
+include_once 'includes/_head.php';
+
 ?>
-    </main>
-        <footer>
-            <div class="coluna1">
-            <div class="linha1">
-                <a>Linha 1, Coluna 1</a>
+    <footer>
+        <div class="separacaoFooter">
+            <div class="navegacao">
+                <a>a</a>
             </div>
-            <div class="linha2">
+            <div class="redesSociais">
+                <a>a</a>
             </div>
-            <div class="linha3">
-                <div class="subcol1">
-                    <a><img src="imagens/instagram.png" id="logoInsta"></a>
-                    <img src="imagens/facebook.png" id="logoFace">
-                    <img src="imagens/youtube.png" id="logoYT">
-                </div>
+            <div class="contato">
+                <a>a</a>
             </div>
-            </div>
-            <div class="coluna2">
-                <div class="linha">
-                    <a>Linha 1, Coluna 2</a>    
-                </div>
-                <div class="linha">
-                    <a>Linha 2, Coluna 2</a>    
-                </div>
-                <div class="linha">
-                    <a>Linha 3, Coluna 2</a>    
-                </div>
-                <div class="linha">
-                    <a>Linha 4, Coluna 2</a>    
-                </div>
-            </div>
-            <div class="coluna3">
-                <div class="linha">
-                    <a>Linha 1, Coluna 3</a>
-                </div>
-                <div class="linha">
-                    <a>Linha 2, Coluna 3</a>
-                </div>
-                <div class="linha">
-                    <a>Linha 3, Coluna 3</a>
-                </div>
-                <div class="linha">
-                    <a>Linha 4, Coluna 3</a>    
-                </div>
-            </div>
-        </footer>
-    </body>
-    <script src="script.js"></script>
+        </div>
+    </footer>
+  </body>
 </html>
