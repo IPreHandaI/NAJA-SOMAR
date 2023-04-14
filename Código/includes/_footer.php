339143764c447fa53ed15f0a400e14bbe806fcb8
Ana Luiza <yanaluiza26y@gmail.com>
2023-04-14 20:43:28 -0300

    footerNovo

--- Código/includes/_footer.php
+++ Código/includes/_footer.php
@@ -1,20 +1,45 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
 
-include_once 'includes/_head.php';
-
-?>
     <footer>
         <div class="separacaoFooter">
             <div class="navegacao">
-                <a>a</a>
+                <h3>Navegação</h3>
+                <a id="aNav">Home</a>
+                <a id="aNav">SOMAR</a>
+                <a id="aNav">Contato</a>
+                <a id="aNav">ODS</a>
             </div>
-            <div class="redesSociais">
-                <a>a</a>
+            <div class="desenvolvedores">
+                <h3>Desenvolvedores</h3>
+                <a id="aDes">Ana Luiza Santos</a>
+                <a id="aDes">Arthur Da Silva</a>
+                <a id="aDes">João Gabriel Gotardo</a>
+                <a id="aDes">Júlia Magalhães</a>
+                <a id="aDes">Nicolas Diovani</a>
             </div>
             <div class="contato">
-                <a>a</a>
+                <h3>Contato</h3>
+                <img id="imgCon" src="imagens/facebook.png">
+                <img id="imgCon" src="imagens/instagram.webp">
+                <img id="imgCon" src="imagens/whatsapp.png">
+                <img id="imgCon" src="imagens/youtube.png">
             </div>
+        </div>
+        <div id="date">
+            <span id="span2023">2023</span>
         </div>
     </footer>
   </body>
+  <script src="script.js"></script>
 </html>
--- Código/login.php
+++ Código/login.php
@@ -1,18 +1,27 @@
 <?php
-include_once 'includes/_head.php';
 include_once 'includes/_header.php';
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../CSS/login.css">
+    <title>Document</title>
+</head>
+<body>
     <main>
         <div id="box-pai">
             <h1 style="color:#8B8B8B;" >Login</h1>
             <h3 style="color:#8B8B8B;">Lorem ipsum dolor, sit amet consectetur </h3>
-            <input type="text" name="" id="" class="caixa-texto">
-            <input type="password" name="" id="" class="caixa-texto">
-            <input type="button" value="clicar" id="botão">
+            <input type="text" name="" id="" class="caixa-texto" placeholder="Nome de usuário ou e-mail " />
+            <input type="password" name="" id="" class="caixa-texto" placeholder="Senha" />
+            <button id="botao">Entrar</button>
             <hr>
             <p>or with</p>
-            <input type="text" name="" id="" class="caixa-texto">
-            <input type="password" name="" id="" class="caixa-texto">
+            <img id="logar" src="imagens/facebook.png">
+            <img id="logar" src="imagens/instagram.webp">
             <h6 style="color:#8B8B8B; margin-top: 15px;">Don’t have an accont yet? <a style="color:#444444;">Sing Up</a></h6>
         </div>
     </main>
