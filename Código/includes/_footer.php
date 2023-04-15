40f80be3165a0280a0d3eeedfe70661395fa11c0
nicolasdiovani <93234639+nicolasdiovani@users.noreply.github.com>
2023-04-14 22:04:37 -0300

    Update _footer.php

--- Código/includes/_footer.php
+++ Código/includes/_footer.php
@@ -12,33 +12,32 @@
 </html>
 
     <footer>
-        <div class="separacaoFooter">
-            <div class="navegacao">
+        <div id="footer-box">
+            <div>
                 <h3>Navegação</h3>
-                <a id="aNav">Home</a>
-                <a id="aNav">SOMAR</a>
-                <a id="aNav">Contato</a>
-                <a id="aNav">ODS</a>
+                <a href="">Home</a>
+                <a href="">SOMAR</a>
+                <a href="">Contato</a>
+                <a href="">ODS</a>
             </div>
-            <div class="desenvolvedores">
+            <div>
                 <h3>Desenvolvedores</h3>
-                <a id="aDes">Ana Luiza Santos</a>
-                <a id="aDes">Arthur Da Silva</a>
-                <a id="aDes">João Gabriel Gotardo</a>
-                <a id="aDes">Júlia Magalhães</a>
-                <a id="aDes">Nicolas Diovani</a>
+                <a href="">Ana Luiza Santos</a>
+                <a href="">Arthur Da Silva</a>
+                <a href="">João Gabriel Gotardo</a>
+                <a href="">Júlia Magalhães</a>
+                <a href="">Nicolas Diovani</a>
             </div>
-            <div class="contato">
+            <div>
                 <h3>Contato</h3>
-                <img id="imgCon" src="imagens/facebook.png">
-                <img id="imgCon" src="imagens/instagram.webp">
-                <img id="imgCon" src="imagens/whatsapp.png">
-                <img id="imgCon" src="imagens/youtube.png">
+                <a href=""><img src="facebook.png"></a>
+                <a href=""><img src="instagram.webp"></a>
+                <a href=""><img src="whatsapp.png"></a>
+                <a href=""><img src="youtube.png"></a>
             </div>
         </div>
-        <div id="date">
-            <span id="span2023">2023</span>
-        </div>
+        <p>2023</p>
+        <div style="background-color:rgb(253, 202, 33);height: 1.5px;width: 100%;"></div>
     </footer>
   </body>
   <script src="script.js"></script>
