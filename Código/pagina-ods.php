baf8c3cb6ea990e8f215d9c539adb3dc49244e2e
nicolasdiovani <93234639+nicolasdiovani@users.noreply.github.com>
2023-04-17 23:45:54 -0300

    Update pagina-ods.php

--- Código/pagina-ods.php
+++ Código/pagina-ods.php
@@ -3,11 +3,38 @@ $ido=$_GET["butao"];
 include_once 'includes/_header.php';
 include_once 'includes/_dados.php'
 ?>
-<div id="super-box">
-<article>
-        <img src="<?php echo $ods[$ido]["link"] ?>">
-        <h3><?php echo $ods[$ido]["h3"] ?></h3>
-    </article>
+    <link rel="stylesheet" href="../CSS/pagina-ods.css">
+    <div id="box-pai">
+        <div class="box-filha">
+            <img class="img-1" src="<?php echo $ods[$ido]["link"] ?>">
+        <div class="box-neta">
+            <h3>Lorem ipsum</h3>
+            <h2>Lorem ipsum dolor sit amet, consectetur</h2>
+            <div>
+                <h4>Lorem ipsum</h4>
+            </div>
+        </div>
+    </div>
+    <div class="box-filha">
+        <img class="img-1" src="<?php echo $ods[$ido]["link"] ?>">
+        <div class="box-neta">
+            <h3>Lorem ipsum</h3>
+            <h2>Lorem ipsum dolor sit amet, consectetur</h2>
+            <div>
+                <h4>Lorem ipsum</h4>
+            </div>
+        </div>
+    </div>
+    <div class="box-filha">
+        <img class="img-1" src="<?php echo $ods[$ido]["link"] ?>">
+        <div class="box-neta">
+            <h3>Lorem ipsum</h3>
+            <h2>Lorem ipsum dolor sit amet, consectetur</h2>
+            <div>
+                <h4>Lorem ipsum</h4>
+            </div>
+        </div>
+    </div>
 </div>
 <?php
 include_once 'includes/_footer.php';
