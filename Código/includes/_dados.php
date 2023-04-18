41cb6409ede745769b9a589764892dead96b771a
nicolasdiovani <93234639+nicolasdiovani@users.noreply.github.com>
2023-04-17 22:31:59 -0300

    Add files via upload

---
+++ Código/includes/_dados.php
@@ -0,0 +1,140 @@
+<?php
+$ods=array(
+    0=>array(
+        "Titulo"=>"Brownie classic",
+        "link"=>"imagens/1.png",
+        "h3"=>"Objetivo de Desenvolvimento Sustentável",
+        "num"=>"1",
+        "h1"=>"Erradicação da pobreza",
+        "h2"=>"Erradicar a pobreza em todas as formas e em todos os lugares"
+    ),
+    1=>array(
+        "Titulo"=>"Brownie classic",
+        "link"=>"imagens/2.png",
+        "h3"=>"",
+        "num"=>"2",
+        "h1"=>"",
+        "h2"=>""
+    ),
+    2=>array(
+        "Titulo"=>"Brownie classic",
+        "link"=>"imagens/3.jpg",
+        "h3"=>"",
+        "num"=>"3",
+        "h1"=>"",
+        "h2"=>""
+    ),
+    3=>array(
+        "Titulo"=>"Brownie classic",
+        "link"=>"imagens/4.webp",
+        "h3"=>"",
+        "num"=>"4",
+        "h1"=>"",
+        "h2"=>""
+    ),
+    4=>array(
+        "Titulo"=>"Brownie classic",
+        "link"=>"imagens/5.gif",
+        "h3"=>"",
+        "num"=>"5",
+        "h1"=>"",
+        "h2"=>""
+    ),
+    5=>array(
+        "Titulo"=>"Brownie classic",
+        "link"=>"imagens/6.png",
+        "h3"=>"",
+        "num"=>"6",
+        "h1"=>"",
+        "h2"=>""
+    ),
+    6=>array(
+        "Titulo"=>"Brownie classic",
+        "link"=>"imagens/7.png",
+        "h3"=>"",
+        "num"=>"7",
+        "h1"=>"",
+        "h2"=>""
+    ),
+    7=>array(
+        "Titulo"=>"Brownie classic",
+        "link"=>"imagens/8.png",
+        "h3"=>"",
+        "num"=>"8",
+        "h1"=>"",
+        "h2"=>""
+    ),
+    8=>array(
+        "Titulo"=>"Brownie classic",
+        "link"=>"imagens/9.png",
+        "h3"=>"",
+        "num"=>"9",
+        "h1"=>"",
+        "h2"=>""
+    ),
+    9=>array(
+        "Titulo"=>"Brownie classic",
+        "link"=>"imagens/10.webp",
+        "h3"=>"",
+        "num"=>"10",
+        "h1"=>"",
+        "h2"=>""
+    ),
+    10=>array(
+        "Titulo"=>"Brownie classic",
+        "link"=>"imagens/11.jpg",
+        "h3"=>"",
+        "num"=>"11",
+        "h1"=>"",
+        "h2"=>""
+    ),
+    11=>array(
+        "Titulo"=>"Brownie classic",
+        "link"=>"imagens/12.png",
+        "h3"=>"",
+        "num"=>"12",
+        "h1"=>"",
+        "h2"=>""
+    ),
+    12=>array(
+        "Titulo"=>"Brownie classic",
+        "link"=>"imagens/13.png",
+        "h3"=>"",
+        "num"=>"13",
+        "h1"=>"",
+        "h2"=>""
+    ),
+    13=>array(
+        "Titulo"=>"Brownie classic",
+        "link"=>"imagens/14.png",
+        "h3"=>"",
+        "num"=>"14",
+        "h1"=>"",
+        "h2"=>""
+    ),
+    14=>array(
+        "Titulo"=>"Brownie classic",
+        "link"=>"imagens/15.png",
+        "h3"=>"",
+        "num"=>"15",
+        "h1"=>"",
+        "h2"=>""
+    ),
+    15=>array(
+        "Titulo"=>"Brownie classic",
+        "link"=>"imagens/16.webp",
+        "h3"=>"",
+        "num"=>"16",
+        "h1"=>"",
+        "h2"=>""
+    ),
+    16=>array(
+        "Titulo"=>"Brownie classic",
+        "link"=>"imagens/17.png",
+        "h3"=>"",
+        "num"=>"17",
+        "h1"=>"",
+        "h2"=>""
+    )
+    );
+?>
