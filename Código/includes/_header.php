63cf4b622bd0376e8900ace200845911512441f5
nicolasdiovani <93234639+nicolasdiovani@users.noreply.github.com>
2023-04-17 22:51:21 -0300

    Update _header.php

--- Código/includes/_header.php
+++ Código/includes/_header.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../CSS/header.css">
+    <style>*{margin:0;padding:0;}</style>
     <title>Document</title>
 </head>
 <body>
