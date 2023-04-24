c365aaccd367b33fe47103f0f41a39429cd1beae
nicolasdiovani <93234639+nicolasdiovani@users.noreply.github.com>
2023-04-24 16:51:48 -0300

    16:51

--- Código/includes/_header.php
+++ Código/includes/_header.php
@@ -11,7 +11,6 @@
 <body>
 
     <header>
-        <div>
             <ul class="rede">
                 <li id="redes">
                     <a href="https://www.fecomercio-rs.org.br/home" target="_blank">FECOMÉRCIO</a>
@@ -26,56 +25,12 @@
                     <a href="https://www.portaldocomercio.org.br" target="_blank">CNC</a>
                 </li>
             </ul>
-        </div>
-        <div class="colsheader">
-            <div class="headercol headercol1" >
-                <div id="logo-centro">
-                    <img id="logo" src="imagens/logo.png">
-                </div>
+        <nav>
+            <img src="imagens/logo.png" alt="">
+            <div>
+                <button>Opção 1</button>
+                <button>Opção 2</button>
             </div>
-            <div class="headercol headercol2">
-                <div class="dropdown1">
-                <div class="dropdown-header1">
-                    <a>Selecione</a>
-                </div>
-                    <div class="dropdown-options2">
-                        <div class="dropdown-option2 option1" value="opcao1" style="width:80%;">
-                            <p id="p">Opção 1</p>    
-                        </div>
-                        <div class="dropdown-option2 option2" value="opcao2">
-                            <p>Opção 2</p>
-                        </div>
-                        <div class="dropdown-option2 option3" value="opcao3">
-                            <p>Opção 3</p>
-                        </div>
-                        <div class="dropdown-option2 option4" value="opcao4">
-                            <p>Opção 4</p>
-                        </div>
-                    </div>            
-                </div>
-            </div>
-            <div class="headercol headercol2">
-                <div class="dropdown2">
-                    <div class="dropdown-header2">
-                        <a>Selecione</a>
-                    </div>
-                    <div class="dropdown-options2">
-                        <div class="dropdown-option2 option1" value="opcao1">
-                            <p id="p">Opção 1</p>    
-                        </div>
-                        <div class="dropdown-option2 option2" value="opcao2">
-                            <p>Opção 2</p>
-                        </div>
-                        <div class="dropdown-option2 option3" value="opcao3">
-                            <p>Opção 3</p>
-                        </div>
-                        <div class="dropdown-option2 option4" value="opcao4">
-                            <p>Opção 4</p>
-                        </div>
-                    </div>            
-                </div>
-            </div>
-        </div>
-
+        </nav>
     </header>
     <main>
