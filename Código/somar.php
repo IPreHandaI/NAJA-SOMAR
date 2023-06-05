ccbde8efe8b5cdba44b79012aa3b8eb72e69fa81
Ana Luiza <yanaluiza26y@gmail.com>
2023-06-04 23:11:14 -0300

    .

--- Código/somar.php
+++ Código/somar.php
@@ -13,14 +13,30 @@ include_once 'includes/_header.php';
                 <h1>Lorem ipsum dolor sit amet.</h1>
                 <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed commodi dignissimos accusamus ad illo ea.</h3>
             </div>
+            <h1>Lorem ipsum dolor sit amet.</h1>
+            <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum temporibus facere animi laboriosam? Facilis, officia!</h3> 
             <div class="button-container">
-                <button id="botao1" class="meu-botao">aaa</button>
-                <button id="botao2" class="meu-botao">aaa</button>
+                <button id="botao1" class="meu-botao">Clique Aqui</button>
+                <button id="botao2" class="meu-botao">Clique Aqui</button>
             </div>
         </div>
         <div id="col2" class="col">
-            <img id="imgEsquerda" src="imagens/imgRetrato.jpg">
-            <img id="imgDireita" src="imagens/imgRetrato2.jpg">
+            <img id="imgEsquerda" src="imagens/retrato.jpg">
+            <img id="imgDireita" src="imagens/retrato.jpg">
+        </div>
+    </div>
+    <div id="divQuadro">
+        <div id="quadro">
+            <div class="row">
+                <div class="col-6">
+                    <div id="text">
+                    <h2>Lorem ipsum dolor sit amet.</h2>
+                </div>
+            </div>
+            
+                
+            </div>
         </div>
     </div>
 </div>
+
